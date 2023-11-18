5d50eee9e572d8006a8ecc7b3033c04ab6769a4a
Galland-Mael <maelgalland.71@gmail.com>
2023-11-18 17:44:10 +0100

    dump database + config

--- src/main/resources/db/migration/V20231111184413__create_table_firmware_product.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE firmware_product
-(
-    id      SERIAL      NOT NULL,
-    date    DATE        NOT NULL,
-    version VARCHAR(10) NOT NULL,
-    data    VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-)
---
+++ src/main/resources/db/migration/V20231111185440__insert_into_quality.sql
@@ -0,0 +1,8 @@
+INSERT INTO quality
+VALUES (4, 'Board operator'),
+       (1, 'Cinematographer'),
+       (5, 'Electrician'),
+       (2, 'Gaffer'),
+       (3, 'Light designer'),
+       (7, 'Other'),
+       (6, 'Rental house');
