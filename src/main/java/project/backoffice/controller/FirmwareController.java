cc5a17bd460cc16b3b66bc3e54d8b8e82f9576e8
Galland-Mael <maelgalland.71@gmail.com>
2024-02-16 14:46:32 +0100

    swagger

--- src/main/java/project/backoffice/controller/FirmwareController.java
+++ src/main/java/project/backoffice/controller/FirmwareController.java
@@ -1,12 +1,18 @@
 package project.backoffice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.backoffice.dto.FirmwareDTO;
 import project.backoffice.dto.FirmwareVersionDTO;
 import project.backoffice.entity.Firmware;
+import project.backoffice.exception.ApiException;
+import project.backoffice.exception.MessageExceptionEnum;
 import project.backoffice.service.FirmwareService;
 
 @AllArgsConstructor
@@ -16,12 +22,12 @@ public class FirmwareController {
 private FirmwareService firmwareService;
 
     @GetMapping("/get-latest")
-    public ResponseEntity<FirmwareVersionDTO> getLastestFirmwareVersion() throws JsonProcessingException {
+    public ResponseEntity<FirmwareVersionDTO> getLastestFirmwareVersion() {
         return ResponseEntity.ok(firmwareService.getLatestFirmwareVersion());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FirmwareDTO> getFirmwareById(@PathVariable Long id) throws JsonProcessingException {
+    public ResponseEntity<FirmwareDTO> getFirmwareById(@PathVariable Long id) {
         return ResponseEntity.ok(firmwareService.getFirmwareById(id));
     }
 }
